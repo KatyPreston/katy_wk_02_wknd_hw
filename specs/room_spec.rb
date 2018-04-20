d91321e2cbd37095097694979a60568ddd407ff5
KatyPreston <prestonkj@hotmail.com>
2018-04-20 12:55:31 +0100

    all classes set up

--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -1,21 +1,22 @@
 require("minitest/autorun")
 require("minitest/rg")
+require_relative("../room")
 require_relative("../guest")
 require_relative("../song")
-require_relative("../room")
 
-class RoomTest
+
+class RoomTest < MiniTest::Test
 
   def setup
-    @room = Room.new("Blue", 10, [], ["The Frug", "Mmm Bop", "Sk8er Boi"])
+    @room = Room.new("Blue", 10, ["Blondie", "Stevie"], ["The Frug", "Mmm Bop", "Sk8er Boi"])
   end
 
   def test_name_of_room
     assert_equal("Blue", @room.name)
   end
-  # 
-  # def test_capacity
-  #   assert_equal(10, @room.capacity)
-  # end
+  
+  def test_capacity
+    assert_equal(10, @room.capacity)
+  end
 
 end
